2c92a4c978d222578ae02ae9d383a9a95a2cfce4
Samyusuf101 <samyusuf101@gmail.com>
2022-08-30 04:53:04 -0700

    created header.tsx in components

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <Link href="/">
+          <div className="r
