a20bdd7bb65de346ca967d2ab322e498e719a426
Sam Yusuf <samyusuf101@gmail.com>
2023-06-30 19:52:32 +0100

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -18,7 +18,7 @@ function Header() {
       <div className="flex items-center justify-center md:w-1/5">
         <Link href="/">
           <div
-            className="relative h-10 w-5 cursor-pointer 
+            className="relative h-10 w-6 cursor-pointer 
           opacity-75 transition hover:opacity-100"
           >
             <Image
